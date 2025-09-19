db0b16625bf3bd8378fbf6cd21bd221ae8ae6c3e
kollisey1418 <kollisey1418@gmail.com>
2025-09-19 13:25:08 +0200

    GUI 1.1 Added "Show drivers"

--- ui.py
+++ ui.py
@@ -1,49 +1,80 @@
 import tkinter as tk
 from tkinter import ttk, messagebox, simpledialog, filedialog
+from app_logic import reseption_name, show_drivers
 
-root = tk.Tk()
-root.title("Driver manage")
-root.geometry("400x400")
+def start_app():
+    root = tk.Tk()
+    root.title("Driver manage")
+    root.geometry("400x400")
 
-
-
-def action_selection():
-    label = tk.Label(root, text="You can choose 1 from 3 functions:\n"
-                                "1 - Creat a new driver\n"
-                                "2 - Select a shift for a driver\n"
-                                "3 - Show list of drivers\n"
-                                "4 - Show list of drivers with shifts")
-    label.pack()
+    top_frame = tk.Frame(root)
+    top_frame.pack(fill="x", pady=5)
+    center_frame = tk.Frame(root)
+    center_frame.pack(fill="x", pady=15)
 
 
 
+    def add_information(top_frame, on_submition):
+        def submit():
+            first = entry_first.get()
+            last = entry_last.get()
+            surn = entry_surname.get()
+            messagebox.showinfo("Information added", f"{first} {last} {surn}")
+            top.destroy()
+            on_submition(first, last, surn)
 
-def add_information():
-    first = simpledialog.askstring("Name", "Enter driver's first name: ")
-    last = simpledialog.askstring("Last name", "Enter driver's last name: ")
-    surn = simpledialog.askstring("Surname", "Enter driver's surname: ")
-    messagebox.showinfo(f"Driver added:", "{first} {last} {surn}")
-    return first, last, surn
+        top = tk.Toplevel(top_frame)
+        top.geometry("400x400")
+        top.title("Add information")
 
-action_selection()
+        tk.Label(top, text="First name:").pack()
+        entry_first = tk.Entry(top, width=50)
+        entry_first.pack()
 
-btn = tk.Button(root, text="Create a new driver", command=add_information)
-btn.pack(pady=10)
-root.mainloop()
+        tk.Label(top, text="Last name:").pack()
+        entry_last = tk.Entry(top, width=50)
+        entry_last.pack()
+
+        tk.Label(top, text="Surname:").pack()
+        entry_surname = tk.Entry(top, width=50)
+        entry_surname.pack()
+
+        first = entry_first
+        last = entry_last
+        surn = entry_surname
+
+        tk.Button(top, text="Submit", command=submit).pack()
+
+        return first, last, surn
+
+    tk.Button(top_frame, text="Create a new driver", command=lambda: add_information(top_frame, reseption_name)).pack()
 
 
-def select_shift(rows):
-        print("Select name: ")
-        for idx, row in enumerate(rows, start=1):
-            id, first_name, last_name, surname = row
-            print(f"{idx}. {first_name}. {last_name}. {surname}")
 
-        choice = int(input("Select driver number: "))
-        date_shift = input("Enter driver's date shift: ")
-        assignment = input("Select assignment (1/2): ")
+    def select_shift(rows):
+            print("Select name: ")
+            for idx, row in enumerate(rows, start=1):
+                id, first_name, last_name, surname = row
+                print(f"{idx}. {first_name}. {last_name}. {surname}")
 
-        selected_driver = rows[choice - 1]
-        id, first_name, last_name, surname = selected_driver
-        print(f"{id}. {first_name}. {last_name}. {surname}, {assignment}, {date_shift}")
-        return id, assignment, date_shift
+            choice = int(input("Select driver number: "))
+            date_shift = input("Enter driver's date shift: ")
+            assignment = input("Select assignment (1/2): ")
 
+            selected_driver = rows[choice - 1]
+            id, first_name, last_name, surname = selected_driver
+            print(f"{id}. {first_name}. {last_name}. {surname}, {assignment}, {date_shift}")
+            return id, assignment, date_shift
+
+    def show_rdirers_ui(parent):
+        drivers = show_drivers()
+        top = tk.Toplevel(parent)
+        top.geometry("400x400")
+        top.title("Rdirers")
+        for idx, name in enumerate(drivers, start=1):
+            tk.Label(top, text=f"{idx}. {name}").pack()
+
+
+
+    tk.Button(top_frame, text="Show drivers", command=lambda: show_rdirers_ui(top_frame)).pack()
+    root.mainloop()
